e5a57b1113535ccef03c33f785f0a57c16f04864
jdalton <john.david.dalton@gmail.com>
2025-01-08 10:08:50 -0500

    Undo some lazy loads in scripts

--- scripts/lib/tests.js
+++ scripts/lib/tests.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const util = require('node:util')
+
 const constants = require('@socketregistry/scripts/constants')
 const {
   getModifiedPackagesSync,
@@ -11,18 +13,9 @@ const { LICENSE_GLOB_RECURSIVE, README_GLOB_RECURSIVE } = constants
 let _cliArgs
 function getCliArgs() {
   if (_cliArgs === undefined) {
-    _cliArgs = getUtil().parseArgs(constants.parseArgsConfig).values
+    _cliArgs = util.parseArgs(constants.parseArgsConfig).values
   }
   return _cliArgs
-}
-
-let _util
-function getUtil() {
-  if (_util === undefined) {
-    const id = 'node:util'
-    _util = require(id)
-  }
-  return _util
 }
 
 function isPackageTestingSkipped(eco, regPkgName) {
