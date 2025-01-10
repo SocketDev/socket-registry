41cec13c9f8314bb2db7f9cc057e7c09e402df62
jdalton <john.david.dalton@gmail.com>
2025-01-10 13:36:57 -0500

    Add yocto-spinner types to Spinner

--- registry/lib/spinner.d.ts
+++ registry/lib/spinner.d.ts
@@ -1,9 +1,13 @@
-import { Options, Spinner } from '@socketregistry/yocto-spinner'
+import { Options as YoctoOptions, Spinner as YoctoSpinner } from '@socketregistry/yocto-spinner'
 
 declare const spinnerModule: {
   Spinner: {
-    new (options?: Options): Spinner
-    (options?: Options): Spinner
-  } & Spinner
+    new (options?: YoctoOptions): YoctoSpinner
+    (options?: YoctoOptions): YoctoSpinner
+  } & YoctoSpinner;
+}
+declare namespace spinnerModule {
+  export type Options = YoctoOptions
+  export type Spinner = YoctoSpinner
 }
 export = spinnerModule
