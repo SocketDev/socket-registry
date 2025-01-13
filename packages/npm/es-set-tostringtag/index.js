192e0b2d306f1ab564ab696b0f8bbd98d9a5583d
jdalton <john.david.dalton@gmail.com>
2025-01-13 12:22:56 -0500

    Lint nits

--- packages/npm/es-set-tostringtag/index.js
+++ packages/npm/es-set-tostringtag/index.js
@@ -5,16 +5,15 @@ const TypeErrorCtor = TypeError
 
 module.exports = function setToStringTag(object, value, options) {
   const { force, nonConfigurable } = { __proto__: null, ...options }
-	if (
-    (force !== undefined && typeof force !== 'boolean') ||
-		(nonConfigurable !== undefined && typeof nonConfigurable !== 'boolean')
-	) {
-		throw new TypeErrorCtor('if provided, the `force` and `nonConfigurable` options must be booleans')
-	}
   if (
-    force ||
-    !ObjectHasOwn(object, Symbol.toStringTag)
+    (force !== undefined && typeof force !== 'boolean') ||
+    (nonConfigurable !== undefined && typeof nonConfigurable !== 'boolean')
   ) {
+    throw new TypeErrorCtor(
+      'if provided, the `force` and `nonConfigurable` options must be booleans'
+    )
+  }
+  if (force || !ObjectHasOwn(object, Symbol.toStringTag)) {
     ObjectDefineProperty(object, Symbol.toStringTag, {
       __proto__: null,
       configurable: !nonConfigurable,
