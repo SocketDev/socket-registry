cc4a03aa7508445478842997d1cb8f39b73812a7
jdalton <john.david.dalton@gmail.com>
2025-01-13 12:07:13 -0500

    Update es-set-tostringtag to pass new unit tests

--- packages/npm/es-set-tostringtag/index.js
+++ packages/npm/es-set-tostringtag/index.js
@@ -1,16 +1,26 @@
 'use strict'
 
 const { defineProperty: ObjectDefineProperty, hasOwn: ObjectHasOwn } = Object
+const TypeErrorCtor = TypeError
 
 module.exports = function setToStringTag(object, value, options) {
+  const { force, nonConfigurable } = { __proto__: null, ...options }
+	if (
+    (force !== undefined && typeof force !== 'boolean') ||
+		(nonConfigurable !== undefined && typeof nonConfigurable !== 'boolean')
+	) {
+		throw new TypeErrorCtor('if provided, the `force` and `nonConfigurable` options must be booleans')
+	}
   if (
-    { __proto__: null, ...options }.force ||
+    force ||
     !ObjectHasOwn(object, Symbol.toStringTag)
   ) {
     ObjectDefineProperty(object, Symbol.toStringTag, {
       __proto__: null,
-      configurable: true,
-      value
+      configurable: !nonConfigurable,
+      enumerable: false,
+      value,
+      writable: false
     })
   }
 }
