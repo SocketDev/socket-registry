910ebe469d2d0a2f7ebcd1e78728f0dae4fa0019
jdalton <john.david.dalton@gmail.com>
2025-01-29 22:02:13 -0600

    Turn import-x/no-unresolved off

--- eslint.config.js
+++ eslint.config.js
@@ -138,7 +138,8 @@ function getImportXFlatConfigs(isEsm) {
         // TypeScript compilation already ensures that named imports exist in
         // the referenced module.
         'import-x/named': 'off',
-        'import-x/no-named-as-default-member': 'off'
+        'import-x/no-named-as-default-member': 'off',
+        'import-x/no-unresolved': 'off'
       }
     }
   }
