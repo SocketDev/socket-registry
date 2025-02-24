9692777efddea90f68846fea9cb3d5c9a1b8fd45
jdalton <john.david.dalton@gmail.com>
2025-02-24 11:10:24 -0500

    Fix spinner type definition

--- registry/lib/spinner.d.ts
+++ registry/lib/spinner.d.ts
@@ -1,39 +1,41 @@
 import {
-  Options as YoctoOptions,
-  Spinner as YoctoSpinner
+  Color,
+  Options,
+  Spinner as YoctoSpinner,
+  SpinnerStyle
 } from '@socketregistry/yocto-spinner/index.cjs'
 import { Remap } from './objects'
 
-declare type Options = YoctoOptions
-declare type Spinner = YoctoSpinner
+declare type Spinner = Remap<
+  Omit<
+    YoctoSpinner,
+    'error' | 'info' | 'start' | 'stop' | 'success' | 'warning'
+  > & {
+    error(text?: string | undefined, ...extras: any[]): Spinner
+    errorAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    getText(): string
+    info(text?: string | undefined, ...extras: any[]): Spinner
+    infoAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    log(text?: string | undefined, ...extras: any[]): Spinner
+    logAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    setText(text?: string | undefined): Spinner
+    start(text?: string | undefined, ...extras: any[]): Spinner
+    stop(finalText?: string | undefined, ...extras: any[]): Spinner
+    success(text?: string | undefined, ...extras: any[]): Spinner
+    successAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    warn(text?: string | undefined, ...extras: any[]): Spinner
+    warnAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    warning(text?: string | undefined, ...extras: any[]): Spinner
+    warningAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  }
+>
 declare const SpinnerModule: {
-  Spinner: Remap<
-    Omit<
-      Spinner,
-      'error' | 'info' | 'start' | 'stop' | 'success' | 'warning'
-    > & {
-      new (options?: Options): Spinner
-      (options?: Options): Spinner
-      error(text?: string | undefined, ...extras: any[]): Spinner
-      errorAndStop(text?: string | undefined, ...extras: any[]): Spinner
-      getText(): string
-      info(text?: string | undefined, ...extras: any[]): Spinner
-      infoAndStop(text?: string | undefined, ...extras: any[]): Spinner
-      log(text?: string | undefined, ...extras: any[]): Spinner
-      logAndStop(text?: string | undefined, ...extras: any[]): Spinner
-      setText(text?: string | undefined): Spinner
-      start(text?: string | undefined, ...extras: any[]): Spinner
-      stop(finalText?: string | undefined, ...extras: any[]): Spinner
-      success(text?: string | undefined, ...extras: any[]): Spinner
-      successAndStop(text?: string | undefined, ...extras: any[]): Spinner
-      warn(text?: string | undefined, ...extras: any[]): Spinner
-      warnAndStop(text?: string | undefined, ...extras: any[]): Spinner
-      warning(text?: string | undefined, ...extras: any[]): Spinner
-      warningAndStop(text?: string | undefined, ...extras: any[]): Spinner
-    }
-  >
+  Spinner: Spinner & {
+    new (options?: Options): Spinner
+    (options?: Options): Spinner
+  }
 }
 declare namespace SpinnerModule {
-  export { Options, Spinner }
+  export { Color, Options, SpinnerStyle, Spinner }
 }
 export = SpinnerModule
