e76a6d02e69c4ead5396eab1cf5aefc579a516c6
jdalton <john.david.dalton@gmail.com>
2025-02-24 13:37:46 -0500

    Fix registry packages and spinner type definitions

--- registry/lib/packages.d.ts
+++ registry/lib/packages.d.ts
@@ -7,67 +7,75 @@ import {
 } from 'pacote'
 import { CategoryString } from '../index'
 
-declare class EditablePackageJson extends NPMCliPackageJson {
-  content: Readonly<PackageJson>
-  saveSync: () => void
-}
-declare type Exports = Exclude<PackageJson['exports'], undefined>
-declare type ExtractOptions = PacoteOptions & {
-  dest?: string
-  tmpPrefix?: string
-}
-declare interface LicenseNode {
-  license: string
-  exception?: string
-  inFile?: string
-  plus?: boolean
-}
-declare type NormalizedPackageJson = Omit<PackageJson, 'repository'> & {
-  repository?: Exclude<PackageJson['repository'], string>
-}
-declare type PackageJson = NPMCliPackageJson.Content & {
-  socket?: { categories: CategoryString }
-}
-declare type PacoteOptions = PacoteOptionsRaw & {
-  signal?: AbortSignal
-}
-declare const Packages: {
-  collectIncompatibleLicenses(licenseNodes: LicenseNode[]): LicenseNode[]
-  collectLicenseWarnings(licenseNodes: LicenseNode[]): string[]
-  createPackageJson(
+declare namespace Packages {
+  export class EditablePackageJson extends NPMCliPackageJson {
+    content: Readonly<PackageJson>
+    saveSync: () => void
+  }
+  export type Exports = Exclude<PackageJson['exports'], undefined>
+  export type ExtractOptions = PacoteOptions & {
+    dest?: string
+    tmpPrefix?: string
+  }
+  export interface LicenseNode {
+    license: string
+    exception?: string
+    inFile?: string
+    plus?: boolean
+  }
+  export type NormalizedPackageJson = Omit<PackageJson, 'repository'> & {
+    repository?: Exclude<PackageJson['repository'], string>
+  }
+  export type PackageJson = NPMCliPackageJson.Content & {
+    socket?: { categories: CategoryString }
+  }
+  export type PacoteOptions = PacoteOptionsRaw & {
+    signal?: AbortSignal
+  }
+  export function collectIncompatibleLicenses(
+    licenseNodes: LicenseNode[]
+  ): LicenseNode[]
+  export function collectLicenseWarnings(licenseNodes: LicenseNode[]): string[]
+  export function createPackageJson(
     sockRegPkgName: string,
     directory: string,
     options: PackageJson
   ): PackageJson
-  extractPackage(
+  export function extractPackage(
     pkgNameOrId: string,
     options: ExtractOptions,
     callback: (destPath: string) => Promise<any>
   ): Promise<void>
-  fetchPackageManifest(
+  export function fetchPackageManifest(
     pkgNameOrId: string,
     options?: PacoteOptions
   ): Promise<Awaited<ReturnType<typeof PacoteManifestFn>> | null>
-  fetchPackagePackument(
+  export function fetchPackagePackument(
     pkgNameOrId: string,
     options?: PacoteOptions
   ): Promise<Awaited<ReturnType<typeof PacotePackumentFn>> | null>
-  findTypesForSubpath(
+  export function findTypesForSubpath(
     entryExports: Exports,
     subpath: string
   ): string | undefined
-  getSubpaths(entryExports: Exports): string[]
-  isBlessedPackageName(name: any): boolean
-  isConditionalExports(entryExports: Exports): boolean
-  isGitHubTgzSpec(spec: string, where?: string | undefined): boolean
-  isGitHubUrlSpec(spec: string, where?: string | undefined): boolean
-  isSubpathExports(entryExports: Exports): boolean
-  isValidPackageName(name: any): boolean
-  normalizePackageJson(
+  export function getSubpaths(entryExports: Exports): string[]
+  export function isBlessedPackageName(name: any): boolean
+  export function isConditionalExports(entryExports: Exports): boolean
+  export function isGitHubTgzSpec(
+    spec: string,
+    where?: string | undefined
+  ): boolean
+  export function isGitHubUrlSpec(
+    spec: string,
+    where?: string | undefined
+  ): boolean
+  export function isSubpathExports(entryExports: Exports): boolean
+  export function isValidPackageName(name: any): boolean
+  export function normalizePackageJson(
     pkgJson: PackageJson,
     options?: { preserve?: string[] }
   ): NormalizedPackageJson
-  packPackage(
+  export function packPackage(
     spec: string,
     options?: PacoteOptions & {
       args?: string[]
@@ -82,59 +90,53 @@ declare const Packages: {
       stdioString?: boolean
     }
   ): Promise<Awaited<ReturnType<typeof PacoteTarballFn>>>
-  readPackageJson(
+  export function readPackageJson(
     filepath: string,
     options: { editable: true; preserve?: string[] }
   ): Promise<EditablePackageJson>
-  readPackageJson(
+  export function readPackageJson(
     filepath: string,
     options?: { editable?: false; preserve?: string[] }
   ): Promise<PackageJson>
-  readPackageJsonSync(
+  export function readPackageJsonSync(
     filepath: string,
     options: { editable: true; preserve?: string[] }
   ): EditablePackageJson
-  readPackageJsonSync(
+  export function readPackageJsonSync(
     filepath: string,
     options?: { editable?: false; preserve?: string[] }
   ): PackageJson
-  resolveEscapedScope(sockRegPkgName: string): string
-  resolveGitHubTgzUrl(pkgNameOrId: string, where: string): Promise<string>
-  resolveOriginalPackageName(sockRegPkgName: string): string
-  resolvePackageJsonDirname(filepath: string): string
-  resolvePackageJsonEntryExports(entryExports: any): Exports | undefined
-  resolvePackageJsonPath(filepath: string): string
-  resolvePackageLicenses(
+  export function resolveEscapedScope(sockRegPkgName: string): string
+  export function resolveGitHubTgzUrl(
+    pkgNameOrId: string,
+    where: string
+  ): Promise<string>
+  export function resolveOriginalPackageName(sockRegPkgName: string): string
+  export function resolvePackageJsonDirname(filepath: string): string
+  export function resolvePackageJsonEntryExports(
+    entryExports: any
+  ): Exports | undefined
+  export function resolvePackageJsonPath(filepath: string): string
+  export function resolvePackageLicenses(
     licenseFieldValue: string,
     where: string
   ): LicenseNode[]
-  resolvePackageName(
+  export function resolvePackageName(
     purlObj: {
       name: string
       namespace?: string | undefined
     },
     delimiter?: string | undefined
   ): string
-  resolveRegistryPackageName(pkgName: string): string
-  toEditablePackageJson(
+  export function resolveRegistryPackageName(pkgName: string): string
+  export function toEditablePackageJson(
     pkgJson: PackageJson,
     options: { path?: string; preserve?: string[] }
   ): Promise<EditablePackageJson>
-  toEditablePackageJsonSync(
+  export function toEditablePackageJsonSync(
     pkgJson: PackageJson,
     options: { path?: string; preserve?: string[] }
   ): EditablePackageJson
-  unescapeScope(escapedScope: string): string
-}
-declare namespace Packages {
-  export type EditablePackageJson = typeof EditablePackageJson
-  export {
-    Exports,
-    ExtractOptions,
-    LicenseNode,
-    NormalizedPackageJson,
-    PackageJson,
-    PacoteOptions
-  }
+  export function unescapeScope(escapedScope: string): string
 }
 export = Packages
--- registry/lib/spinner.d.ts
+++ registry/lib/spinner.d.ts
@@ -1,41 +1,45 @@
 import {
-  Color,
-  Options,
+  Color as YoctoColor,
+  Options as YoctoOptions,
   Spinner as YoctoSpinner,
-  SpinnerStyle
+  SpinnerStyle as YoctoSpinnerStyle
 } from '@socketregistry/yocto-spinner/index.cjs'
 import { Remap } from './objects'
 
-declare type Spinner = Remap<
+declare type SpinnerType = Remap<
   Omit<
     YoctoSpinner,
     'error' | 'info' | 'start' | 'stop' | 'success' | 'warning'
   > & {
-    error(text?: string | undefined, ...extras: any[]): Spinner
-    errorAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    error(text?: string | undefined, ...extras: any[]): SpinnerType
+    errorAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
     getText(): string
-    info(text?: string | undefined, ...extras: any[]): Spinner
-    infoAndStop(text?: string | undefined, ...extras: any[]): Spinner
-    log(text?: string | undefined, ...extras: any[]): Spinner
-    logAndStop(text?: string | undefined, ...extras: any[]): Spinner
-    setText(text?: string | undefined): Spinner
-    start(text?: string | undefined, ...extras: any[]): Spinner
-    stop(finalText?: string | undefined, ...extras: any[]): Spinner
-    success(text?: string | undefined, ...extras: any[]): Spinner
-    successAndStop(text?: string | undefined, ...extras: any[]): Spinner
-    warn(text?: string | undefined, ...extras: any[]): Spinner
-    warnAndStop(text?: string | undefined, ...extras: any[]): Spinner
-    warning(text?: string | undefined, ...extras: any[]): Spinner
-    warningAndStop(text?: string | undefined, ...extras: any[]): Spinner
+    info(text?: string | undefined, ...extras: any[]): SpinnerType
+    infoAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
+    log(text?: string | undefined, ...extras: any[]): SpinnerType
+    logAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
+    setText(text?: string | undefined): SpinnerType
+    start(text?: string | undefined, ...extras: any[]): SpinnerType
+    stop(finalText?: string | undefined, ...extras: any[]): SpinnerType
+    success(text?: string | undefined, ...extras: any[]): SpinnerType
+    successAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
+    warn(text?: string | undefined, ...extras: any[]): SpinnerType
+    warnAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
+    warning(text?: string | undefined, ...extras: any[]): SpinnerType
+    warningAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
   }
 >
+
 declare const SpinnerModule: {
-  Spinner: Spinner & {
-    new (options?: Options): Spinner
-    (options?: Options): Spinner
-  }
+  Spinner: {
+    new (options?: YoctoOptions): SpinnerType
+    (options?: YoctoOptions): SpinnerType
+  } & SpinnerType
 }
 declare namespace SpinnerModule {
-  export { Color, Options, SpinnerStyle, Spinner }
+  export type Color = YoctoColor
+  export type Options = YoctoOptions
+  export type Spinner = SpinnerType
+  export type SpinnerStyle = YoctoSpinnerStyle
 }
 export = SpinnerModule
