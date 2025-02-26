959659ae95715ebd74184bea774f49840929d430
jdalton <john.david.dalton@gmail.com>
2025-02-25 20:06:41 -0500

    Add NODE_ENV to constants

--- registry/lib/constants.js
+++ registry/lib/constants.js
@@ -424,7 +424,7 @@ const LAZY_SUPPORTS_PROCESS_SEND = () =>
 
 const LAZY_ENV = () => {
   const {
-    env: { CI, NODE_AUTH_TOKEN, PRE_COMMIT, TAP }
+    env: { CI, NODE_AUTH_TOKEN, NODE_ENV, PRE_COMMIT, TAP }
   } = getProcess()
   return Object.freeze({
     __proto__: null,
@@ -432,7 +432,13 @@ const LAZY_ENV = () => {
     // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
     CI: envAsBoolean(CI),
     // .github/workflows/provenance.yml defines this.
+    // https://docs.github.com/en/actions/use-cases-and-examples/publishing-packages/publishing-nodejs-packages
     NODE_AUTH_TOKEN: envAsString(NODE_AUTH_TOKEN),
+    // NODE_ENV is a recognized convention, but not a built-in Node.js feature.
+    NODE_ENV:
+      envAsString(NODE_ENV).toLowerCase() === 'development'
+        ? 'development'
+        : 'production',
     // PRE_COMMIT is set to '1' by our 'test-pre-commit' script run by the
     // .husky/pre-commit hook.
     PRE_COMMIT: envAsBoolean(PRE_COMMIT),
