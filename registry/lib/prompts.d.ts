2dc3cd535e4b659fef3a4a7686eff08b34c133bd
jdalton <john.david.dalton@gmail.com>
2025-03-01 18:38:51 -0500

    Fix prompts types

--- registry/lib/prompts.d.ts
+++ registry/lib/prompts.d.ts
@@ -5,36 +5,38 @@ import inquirerSearch from '@inquirer/search'
 import inquirerSelect, {
   Separator as InquirerSeparator
 } from '@inquirer/select'
-import { Context } from '@inquirer/type'
+import { Context as InquirerContext } from '@inquirer/type'
 import { Remap } from './objects'
 import { Spinner } from './spinner'
 
-declare type OptionalSpinner = { spinner?: Spinner | undefined }
+declare type Context = Remap<
+  InquirerContext & { spinner?: Spinner | undefined }
+>
 declare type Separator = InquirerSeparator
 declare const Prompts: {
   Separator: typeof InquirerSeparator
   confirm: (
-    config: Remap<Parameters<typeof inquirerConfirm>[0] & OptionalSpinner>,
+    config: Parameters<typeof inquirerConfirm>[0],
     context?: Context | undefined
   ) => ReturnType<typeof inquirerConfirm>
   input: (
-    config: Remap<Parameters<typeof inquirerInput>[0] & OptionalSpinner>,
+    config: Parameters<typeof inquirerInput>[0],
     context?: Context | undefined
   ) => ReturnType<typeof inquirerInput>
   password: (
-    config: Remap<Parameters<typeof inquirerPassword>[0] & OptionalSpinner>,
+    config: Parameters<typeof inquirerPassword>[0],
     context?: Context | undefined
   ) => ReturnType<typeof inquirerPassword>
   search: <Value>(
-    config: Remap<Parameters<typeof inquirerSearch>[0] & OptionalSpinner>,
+    config: Parameters<typeof inquirerSearch>[0],
     context?: Context | undefined
   ) => ReturnType<typeof inquirerSearch<Value>>
   select: <Value>(
-    config: Remap<Parameters<typeof inquirerSelect>[0] & OptionalSpinner>,
+    config: Parameters<typeof inquirerSelect>[0],
     context?: Context | undefined
   ) => ReturnType<typeof inquirerSelect<Value>>
 }
 declare namespace Prompts {
-  export { Separator }
+  export { Context, Separator }
 }
 export = Prompts
