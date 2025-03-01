f0167f64da9232337b2eecf7e8751c88651d28de
jdalton <john.david.dalton@gmail.com>
2025-03-01 16:10:14 -0500

    Add more Logger methods

--- registry/lib/logger.d.ts
+++ registry/lib/logger.d.ts
@@ -1,3 +1,11 @@
+/// <reference types="node" />
+import { Console, ConsoleConstructorOptions } from 'node:console'
+
+declare type LoggerMethods = {
+  [K in keyof Console]: Console[K] extends (...args: infer A) => any
+    ? (...args: A) => Logger
+    : Console[K]
+}
 declare namespace LoggerModule {
   export type LogSymbols = {
     error: string
@@ -5,14 +13,40 @@ declare namespace LoggerModule {
     success: string
     warning: string
   }
-  export class Logger {
+  export interface Task {
+    run<T>(f: () => T): T
+  }
+  export class Logger extends Console {
     static get LOG_SYMBOLS(): LogSymbols
-    constructor()
-    error(...args: any[]): Logger
-    info(...args: any[]): Logger
-    log(...args: any[]): Logger
+    new(
+      stdout: NodeJS.WritableStream,
+      stderr?: NodeJS.WritableStream,
+      ignoreErrors?: boolean
+    ): Logger
+    new(options: ConsoleConstructorOptions): Logger
+    assert: LoggerMethods['assert']
+    clear: LoggerMethods['clear']
+    count: LoggerMethods['count']
+    countReset: LoggerMethods['countReset']
+    createTask(name: string): Task
+    debug: LoggerMethods['debug']
+    dir: LoggerMethods['dir']
+    dirxml: LoggerMethods['dirxml']
+    error: LoggerMethods['error']
+    group: LoggerMethods['group']
+    groupCollapsed: LoggerMethods['groupCollapsed']
+    groupEnd: LoggerMethods['groupEnd']
+    info: LoggerMethods['info']
+    log: LoggerMethods['log']
+    profile: LoggerMethods['profile']
+    profileEnd: LoggerMethods['profileEnd']
     success(...args: any[]): Logger
-    warn(...args: any[]): Logger
+    table: LoggerMethods['table']
+    time: LoggerMethods['time']
+    timeEnd: LoggerMethods['timeEnd']
+    timeLog: LoggerMethods['timeLog']
+    trace: LoggerMethods['trace']
+    warn: LoggerMethods['warn']
   }
   export const logger: Logger
 }
