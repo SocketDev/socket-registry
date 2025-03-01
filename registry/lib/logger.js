4962037791a602fe3030d846ec8c30486fcdc376
jdalton <john.david.dalton@gmail.com>
2025-03-01 16:10:14 -0500

    Add more Logger methods

--- registry/lib/logger.js
+++ registry/lib/logger.js
@@ -1,5 +1,17 @@
 'use strict'
 
+const { construct: ReflectConstruct } = Reflect
+
+let _Console
+function constructConsole(args) {
+  if (_Console === undefined) {
+    // Use non-'node:' prefixed require to avoid Webpack errors.
+    // eslint-disable-next-line n/prefer-node-protocol
+    _Console = require('console').Console
+  }
+  return ReflectConstruct(_Console, args)
+}
+
 let _yoctocolors
 function getYoctocolors() {
   if (_yoctocolors === undefined) {
@@ -8,7 +20,19 @@ function getYoctocolors() {
   return _yoctocolors
 }
 
+const symbolTypeToMethodName = {
+  error: 'error',
+  info: 'info',
+  success: 'log',
+  warning: 'warn'
+}
+
+const privateConsole = new WeakMap()
+
 class Logger {
+  constructor(...args) {
+    privateConsole.set(this, args.length ? constructConsole(args) : console)
+  }
   #symbolApply(symbolType, args) {
     let extras
     let text = args.at(0) ?? ''
@@ -18,9 +42,11 @@ class Logger {
     } else {
       extras = args.slice(1)
     }
-    console.log(`${Logger.LOG_SYMBOLS[symbolType]} ${text}`)
+    const methodName = symbolTypeToMethodName[symbolType]
+    const console = privateConsole.get(this)
+    console[methodName](`${Logger.LOG_SYMBOLS[symbolType]} ${text}`)
     if (extras.length) {
-      console.log(...extras)
+      console[methodName](...extras)
     }
     return this
   }
@@ -48,11 +74,6 @@ class Logger {
     return this.#symbolApply('info', args)
   }
 
-  log(...args) {
-    console.log(...args)
-    return this
-  }
-
   success(...args) {
     return this.#symbolApply('success', args)
   }
@@ -60,6 +81,34 @@ class Logger {
   warn(...args) {
     return this.#symbolApply('warning', args)
   }
+}
+
+const mixinKeys = []
+for (const { 0: key, 1: value } of Object.entries(console)) {
+  if (!Logger.prototype[key] && typeof value === 'function') {
+    mixinKeys.push(key)
+  }
+}
+
+if (mixinKeys.length) {
+  Object.defineProperties(
+    Logger.prototype,
+    Object.fromEntries(
+      mixinKeys.map(key => [
+        key,
+        {
+          __proto__: null,
+          configurable: true,
+          value: function (...args) {
+            const console = privateConsole.get(this)
+            const result = console[key](...args)
+            return result === undefined ? this : result
+          },
+          writable: true
+        }
+      ])
+    )
+  )
 }
 
 const logger = new Logger()
