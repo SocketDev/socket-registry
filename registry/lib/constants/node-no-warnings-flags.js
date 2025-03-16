0a334774930dc31d69ca9c8565a1b425462c0038
jdalton <john.david.dalton@gmail.com>
2025-03-16 17:59:51 -0400

    Fix node-no-warnings-flags

--- registry/lib/constants/node-no-warnings-flags.js
+++ registry/lib/constants/node-no-warnings-flags.js
@@ -1,3 +1,9 @@
 'use strict'
 
-module.exports = ['--disable-warning', 'ExperimentalWarning']
+const SUPPORTS_NODE_DISABLE_WARNING_FLAG = /*@__PURE__*/ require('./supports-node-disable-warning-flag')
+
+module.exports = Object.freeze(
+  SUPPORTS_NODE_DISABLE_WARNING_FLAG
+    ? ['--disable-warning', 'ExperimentalWarning']
+    : ['--no-warnings']
+)
