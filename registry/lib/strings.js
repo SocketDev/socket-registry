38430f8a587708521fa78ee4f9348e255a234808
jdalton <john.david.dalton@gmail.com>
2025-03-31 07:59:10 -0400

    Remove biomeFormat strings helper

--- registry/lib/strings.js
+++ registry/lib/strings.js
@@ -1,72 +1,5 @@
 'use strict'
 
-const DEFAULT_BIOME_CONFIG = {
-  __proto__: null,
-  $schema: './node_modules/@biomejs/biome/configuration_schema.json',
-  formatter: {
-    enabled: true,
-    attributePosition: 'auto',
-    bracketSpacing: true,
-    formatWithErrors: false,
-    indentStyle: 'space',
-    indentWidth: 2,
-    lineEnding: 'lf',
-    lineWidth: 80,
-    useEditorconfig: true
-  },
-  javascript: {
-    formatter: {
-      arrowParentheses: 'asNeeded',
-      attributePosition: 'auto',
-      bracketSameLine: false,
-      bracketSpacing: true,
-      jsxQuoteStyle: 'double',
-      quoteProperties: 'asNeeded',
-      quoteStyle: 'single',
-      semicolons: 'asNeeded',
-      trailingCommas: 'none'
-    }
-  },
-  json: {
-    formatter: {
-      enabled: true,
-      trailingCommas: 'none'
-    },
-    parser: {
-      allowComments: true,
-      allowTrailingCommas: true
-    }
-  }
-}
-
-let _biome
-/*@__NO_SIDE_EFFECTS__*/
-async function getBiome() {
-  if (_biome === undefined) {
-    const { Biome, Distribution } = /*@__PURE__*/ require('@biomejs/js-api')
-    _biome = await Biome.create({
-      distribution: Distribution.NODE
-    })
-  }
-  return _biome
-}
-
-/*@__NO_SIDE_EFFECTS__*/
-async function biomeFormat(str, options) {
-  const {
-    filepath,
-    filePath = filepath,
-    ...biomeConfig
-  } = { __proto__: null, ...options }
-  const biome = await getBiome()
-  biome.applyConfiguration({
-    __proto__: null,
-    ...DEFAULT_BIOME_CONFIG,
-    ...biomeConfig
-  })
-  return biome.formatContent(str, { filePath }).content
-}
-
 /*@__NO_SIDE_EFFECTS__*/
 function indentString(str, count = 1) {
   return str.replace(/^(?!\s*$)/gm, ' '.repeat(count))
@@ -99,7 +32,6 @@ function stripBom(str) {
 }
 
 module.exports = {
-  biomeFormat,
   indentString,
   isNonEmptyString,
   search,
