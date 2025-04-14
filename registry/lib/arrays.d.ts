a3d17bdd25628726ccaad86135d6b59c45b16ea6
jdalton <john.david.dalton@gmail.com>
2025-04-14 08:28:06 -0600

    Accept readonly arrays for array methods

--- registry/lib/arrays.d.ts
+++ registry/lib/arrays.d.ts
@@ -1,6 +1,6 @@
 declare const Arrays: {
-  arrayChunk<T>(arr: T[], size?: number): T[][]
-  arrayUnique<T>(arr: T[]): T[]
+  arrayChunk<T>(arr: T[] | readonly T[], size?: number): T[][]
+  arrayUnique<T>(arr: T[] | readonly T[]): T[]
   joinAnd(arr: string[] | readonly string[]): string
   joinOr(arr: string[] | readonly string[]): string
 }
