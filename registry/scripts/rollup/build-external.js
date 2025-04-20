51c8453bf17e0592bfd0683aa7e314430aa84172
jdalton <john.david.dalton@gmail.com>
2025-04-19 22:21:32 -0600

    Add babel transforms to registry build

---
+++ registry/scripts/rollup/build-external.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const path = require('node:path')
+
+const { rollup } = require('rollup')
+const { glob: tinyGlob } = require('tinyglobby')
+
+const scriptsPath = path.join(__dirname, '..')
+const rootPath = path.join(scriptsPath, '..')
+const configPath = path.join(rootPath, '.config')
+
+const getConfig = require(path.join(configPath, 'rollup.base.config.js'))
+;(async () => {
+  const rootPath = path.join(__dirname, '../..')
+  const srcPath = path.join(rootPath, 'src')
+  const srcExternalPath = path.join(srcPath, 'external')
+  const filepaths = await tinyGlob(['**/*.js'], {
+    absolute: true,
+    cwd: srcExternalPath
+  })
+  await Promise.all(
+    filepaths.map(async filepath => {
+      const relPath = path.relative(srcPath, filepath)
+      const bundle = await rollup(getConfig(filepath))
+      await bundle.write({
+        file: path.join(rootPath, relPath),
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true
+      })
+      console.log(`✅ Built ${relPath}`)
+    })
+  )
+})()
