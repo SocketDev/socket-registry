451723c09a7fe1a952c2a7f72ad86745e5819844
jdalton <john.david.dalton@gmail.com>
2025-05-07 15:16:45 -0700

    Ensure tests work in Node 24

--- scripts/test.js
+++ scripts/test.js
@@ -4,23 +4,28 @@ const constants = require('@socketregistry/scripts/constants')
 const { runBin } = require('@socketsecurity/registry/lib/npm')
 
 void (async () => {
-  await runBin(
-    // Lazily access constants.tapRunExecPath.
-    constants.tapRunExecPath,
-    process.argv.slice(2),
-    {
-      // Lazily access constants.rootPath.
-      cwd: constants.rootPath,
-      stdio: 'inherit',
-      env: {
-        __proto__: null,
-        ...process.env,
-        // Lazily access constants.ENV.
-        TAP_RCFILE: constants.ENV.CI
-          ? // Lazily access constants.tapCiConfigPath and constants.tapConfigPath.
-            constants.tapCiConfigPath
-          : constants.tapConfigPath
+  try {
+    await runBin(
+      // Lazily access constants.tapRunExecPath.
+      constants.tapRunExecPath,
+      process.argv.slice(2),
+      {
+        // Lazily access constants.rootPath.
+        cwd: constants.rootPath,
+        stdio: 'inherit',
+        env: {
+          __proto__: null,
+          ...process.env,
+          // Lazily access constants.ENV.
+          TAP_RCFILE: constants.ENV.CI
+            ? // Lazily access constants.tapCiConfigPath and constants.tapConfigPath.
+              constants.tapCiConfigPath
+            : constants.tapConfigPath
+        }
       }
-    }
-  )
+    )
+  } catch {
+    // Shallow error here since we're running the bin with stdio: 'inherit' and
+    // all stdout and stderr will be logged to the console.
+  }
 })()
