ab35c70cfcda0fe028b856ff1df853e75f5ac0dd
jdalton <john.david.dalton@gmail.com>
2025-05-10 12:23:06 -0700

    Add babel plugin to transform util.parse(...) into new URL(...)

--- registry/.config/babel.config.js
+++ registry/.config/babel.config.js
@@ -2,8 +2,11 @@
 
 const path = require('node:path')
 
+const rootPath = path.join(__dirname, '..')
+const scriptsPath = path.join(rootPath, 'scripts')
+const babelPluginsPath = path.join(scriptsPath, 'babel')
+
 module.exports = {
-  presets: ['@babel/preset-typescript'],
   plugins: [
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-transform-export-namespace-from',
@@ -14,9 +17,10 @@ module.exports = {
         corejs: false,
         helpers: true,
         regenerator: false,
-        version: '^7.26.10'
+        version: '^7.27.1'
       }
     ],
-    path.resolve('./scripts/babel/transform-set-proto-plugin.js')
+    path.join(babelPluginsPath, 'transform-set-proto-plugin.js'),
+    path.join(babelPluginsPath, 'transform-url-parse-plugin.js')
   ]
 }
