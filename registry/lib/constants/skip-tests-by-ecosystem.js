c3ea35384ce1e8b00006fee64a11ad2cf0422144
jdalton <john.david.dalton@gmail.com>
2025-05-21 11:59:55 -0400

    Bump yocto-spinner

--- registry/lib/constants/skip-tests-by-ecosystem.js
+++ registry/lib/constants/skip-tests-by-ecosystem.js
@@ -38,11 +38,7 @@ module.exports = new Map([
       // which is no longer the case.
       // https://github.com/ChALkeR/safer-buffer/issues/16
       // https://github.com/ChALkeR/safer-buffer/blob/v2.1.2/tests.js
-      'safer-buffer',
-      // yocto-spinner has an open issue with its '✖' string containing an invisible
-      // U+FE0F character. Skipping tests until issues/8 is resolved.
-      // https://github.com/sindresorhus/yocto-spinner/issues/8
-      'yocto-spinner'
+      'safer-buffer'
     ])
   ]
 ])
