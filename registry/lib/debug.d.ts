f3049d48858a1c08bc843170d42e87e41fdce60f
jdalton <john.david.dalton@gmail.com>
2025-05-28 13:24:24 +0200

    Make debugFn work without passing a function in

--- registry/lib/debug.d.ts
+++ registry/lib/debug.d.ts
@@ -1,10 +1,7 @@
 declare const Debug: {
   isDebug(): boolean
   debugDir: typeof console.dir
-  debugFn(
-    fn: Function,
-    ...args: Parameters<typeof console.log>
-  ): ReturnType<typeof console.log>
+  debugFn: typeof console.log
   debugLog: typeof console.log
 }
 declare namespace Debug {}
