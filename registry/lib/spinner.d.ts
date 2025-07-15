8bd9951bc279a8b3d7d95d83208178712a41f556
jdalton <john.david.dalton@gmail.com>
2025-07-15 11:41:38 -0400

    Update spinner types

--- registry/lib/spinner.d.ts
+++ registry/lib/spinner.d.ts
@@ -1,61 +1,63 @@
-import {
-  Color as YoctoColor,
-  Options as YoctoOptions,
-  Spinner as YoctoSpinner,
-  SpinnerStyle as YoctoSpinnerStyle
-} from '@socketregistry/yocto-spinner/index.d.cjs'
+/// <reference types="node" />
+import { Writable } from 'node:stream'
 
-import { Remap } from './objects'
-
-declare type SpinnerType = Remap<
-  Omit<
-    YoctoSpinner,
-    | 'dedent'
-    | 'error'
-    | 'indent'
-    | 'info'
-    | 'resetIndent'
-    | 'start'
-    | 'stop'
-    | 'success'
-    | 'warning'
-  > & {
-    debug(text?: string | undefined, ...extras: any[]): SpinnerType
-    debugAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    dedent(spaces?: number | undefined): SpinnerType
-    error(text?: string | undefined, ...extras: any[]): SpinnerType
-    errorAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    fail(text?: string | undefined, ...extras: any[]): SpinnerType
-    failAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    getText(): string
-    indent(spaces?: number | undefined): SpinnerType
-    info(text?: string | undefined, ...extras: any[]): SpinnerType
-    infoAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    log(text?: string | undefined, ...extras: any[]): SpinnerType
-    logAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    resetIndent(): SpinnerType
-    setText(text?: string | undefined): SpinnerType
-    start(text?: string | undefined, ...extras: any[]): SpinnerType
-    stop(finalText?: string | undefined, ...extras: any[]): SpinnerType
-    success(text?: string | undefined, ...extras: any[]): SpinnerType
-    successAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    warn(text?: string | undefined, ...extras: any[]): SpinnerType
-    warnAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-    warning(text?: string | undefined, ...extras: any[]): SpinnerType
-    warningAndStop(text?: string | undefined, ...extras: any[]): SpinnerType
-  }
->
-
+declare type Color =
+  | 'black'
+  | 'blue'
+  | 'cyan'
+  | 'gray'
+  | 'green'
+  | 'magenta'
+  | 'red'
+  | 'white'
+  | 'yellow'
+declare type Options = {
+  readonly color?: Color | undefined
+  readonly spinner?: SpinnerStyle | undefined
+  readonly signal?: AbortSignal | undefined
+  readonly stream?: Writable | undefined
+  readonly text?: string | undefined
+}
+declare type SpinnerStyle = {
+  readonly frames: string[]
+  readonly interval?: number | undefined
+}
+declare type Spinner = {
+  color: Color
+  text: string
+  get isSpinning(): boolean
+  clear(): Spinner
+  debug(text?: string | undefined, ...extras: any[]): Spinner
+  debugAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  dedent(spaces?: number | undefined): Spinner
+  error(text?: string | undefined, ...extras: any[]): Spinner
+  errorAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  fail(text?: string | undefined, ...extras: any[]): Spinner
+  failAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  getText(): string
+  indent(spaces?: number | undefined): Spinner
+  info(text?: string | undefined, ...extras: any[]): Spinner
+  infoAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  log(text?: string | undefined, ...extras: any[]): Spinner
+  logAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  resetIndent(): Spinner
+  setText(text?: string | undefined): Spinner
+  start(text?: string | undefined, ...extras: any[]): Spinner
+  stop(finalText?: string | undefined, ...extras: any[]): Spinner
+  success(text?: string | undefined, ...extras: any[]): Spinner
+  successAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  warn(text?: string | undefined, ...extras: any[]): Spinner
+  warnAndStop(text?: string | undefined, ...extras: any[]): Spinner
+  warning(text?: string | undefined, ...extras: any[]): Spinner
+  warningAndStop(text?: string | undefined, ...extras: any[]): Spinner
+}
 declare const SpinnerModule: {
   Spinner: {
-    new (options?: YoctoOptions): SpinnerType
-    (options?: YoctoOptions): SpinnerType
-  } & SpinnerType
+    new (options?: Options): Spinner
+    (options?: Options): Spinner
+  } & Spinner
 }
 declare namespace SpinnerModule {
-  export type Color = YoctoColor
-  export type Options = YoctoOptions
-  export type Spinner = SpinnerType
-  export type SpinnerStyle = YoctoSpinnerStyle
+  export { Color, Options, Spinner, SpinnerStyle }
 }
 export = SpinnerModule
