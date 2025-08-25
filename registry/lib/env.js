03e643d4a1d06b3c134e73d056801c218791a1b0
jdalton <john.david.dalton@gmail.com>
2025-08-25 17:32:43 -0400

    Add envAsNumber

--- registry/lib/env.js
+++ registry/lib/env.js
@@ -1,10 +1,20 @@
 'use strict'
 
+const { isFinite: NumberIsFinite, parseInt: NumberParseInt } = Number
+
 /*@__NO_SIDE_EFFECTS__*/
 function envAsBoolean(value) {
   return typeof value === 'string'
     ? value.trim() === '1' || value.trim().toLowerCase() === 'true'
     : !!value
+}
+
+/*@__NO_SIDE_EFFECTS__*/
+function envAsNumber(value) {
+  const numOrNaN = NumberParseInt(value, 10)
+  const numMayBeNegZero = NumberIsFinite(numOrNaN) ? numOrNaN : 0
+  // Ensure -0 is treated as 0.
+  return numMayBeNegZero || 0
 }
 
 /*@__NO_SIDE_EFFECTS__*/
@@ -20,5 +30,6 @@ function envAsString(value) {
 
 module.exports = {
   envAsBoolean,
+  envAsNumber,
   envAsString
 }
