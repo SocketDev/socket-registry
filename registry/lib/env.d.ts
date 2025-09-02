457657f43ecbd7141a33a457f4d490c2fb0dd047
jdalton <john.david.dalton@gmail.com>
2025-09-02 16:35:46 -0400

    Add defaultValue params to env helper methods

--- registry/lib/env.d.ts
+++ registry/lib/env.d.ts
@@ -1,7 +1,7 @@
 declare const Env: {
-  envAsBoolean(value: any): boolean
-  envAsNumber(value: any): number
-  envAsString(value: any): string
+  envAsBoolean(value: any, defaultValue?: boolean | undefined): boolean
+  envAsNumber(value: any, defaultValue?: number | undefined): number
+  envAsString(value: any, defaultValue?: string | undefined): string
 }
 declare namespace Env {}
 export = Env
