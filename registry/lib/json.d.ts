3a5d0d75de66b46b23be73672049ecad17088a68
jdalton <john.david.dalton@gmail.com>
2025-09-04 07:19:11 -0400

    Add jsonParse lib helper

---
+++ registry/lib/json.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="node" />
+
+import type { PathLike } from 'node:fs'
+
+declare type JsonArray = JsonValue[] | readonly JsonValue[]
+declare type JsonObject = {
+  [Key in string]: JsonValue
+} & {
+  [Key in string]?: JsonValue | undefined
+}
+declare type JsonParseOptions = {
+  filepath?: PathLike | undefined
+  reviver?: JsonReviver | undefined
+  throws?: boolean | undefined
+}
+declare type JsonPrimitive = string | number | boolean | null
+declare type JsonValue = JsonPrimitive | JsonObject | JsonArray
+declare type JsonReviver = (this: any, key: string, value: any) => any
+declare const Json: {
+  jsonParse: (
+    content: string,
+    options?: JsonParseOptions | undefined
+  ) => JsonValue
+}
+declare namespace Json {
+  export {
+    JsonArray,
+    JsonObject,
+    JsonParseOptions,
+    JsonPrimitive,
+    JsonValue,
+    JsonReviver
+  }
+}
+export = Json
