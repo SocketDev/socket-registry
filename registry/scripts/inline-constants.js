a757ad54d1d3514a9d39257fdaa9d7c4b3628de5
jdalton <john.david.dalton@gmail.com>
2025-09-08 13:01:34 -0400

    Fix build in ci

--- registry/scripts/inline-constants.js
+++ registry/scripts/inline-constants.js
@@ -14,22 +14,55 @@ const libPath = path.join(rootPath, 'lib')
 const constantsPath = path.join(libPath, 'constants')
 const libConstantsJsPath = path.join(constantsPath, 'index.js')
 
+const exportJsonPrimitiveRegExp =
+  /module\.exports *= *(?:null|undefined|\d|'|")/
+
 function isJsonPrimitive(value) {
   return (
     value === null ||
     value === undefined ||
-    typeof value === 'string' ||
-    typeof value === 'number'
+    typeof value === 'number' ||
+    typeof value === 'string'
   )
 }
 
 const excludedKeys = new Set([
+  'ENV',
+  'DARWIN',
   'NODE_VERSION',
   'PACKAGE_DEFAULT_NODE_RANGE',
+  'PACKAGE_DEFAULT_SOCKET_CATEGORIES',
+  'SUPPORTS_NODE_COMPILE_CACHE_API',
+  'SUPPORTS_NODE_COMPILE_CACHE_ENV_VAR',
+  'SUPPORTS_NODE_DISABLE_WARNING_FLAG',
+  'SUPPORTS_NODE_PERMISSION_FLAG',
+  'SUPPORTS_NODE_REQUIRE_MODULE',
+  'SUPPORTS_NODE_RUN',
+  'SUPPORTS_PROCESS_SEND',
+  'UNDEFINED_TOKEN',
+  'WIN32',
+  'abortController',
+  'abortSignal',
+  'copyLeftLicenses',
   'execPath',
+  'ipcObject',
+  'kInternalsSymbol',
+  'lifecycleScriptNames',
+  'maintainedNodeVersions',
+  'nodeDebugFlags',
+  'nodeHardenFlags',
+  'nodeNoWarningsFlags',
   'npmExecPath',
   'npmRealExecPath',
-  'pacoteCachePath'
+  'packumentCache',
+  'pacoteCachePath',
+  'packageExtensions',
+  'parseArgsConfig',
+  'skipTestsByEcosystem',
+  'spinner',
+  'tsLibsAvailable',
+  'tsTypesAvailable',
+  'win32EnsureTestsByEcosystem'
 ])
 
 ;(async () => {
@@ -39,6 +72,12 @@ const excludedKeys = new Set([
 
   for (const key of Object.keys(constantsObj)) {
     if (excludedKeys.has(key)) {
+      continue
+    }
+    const keyPath = path.join(constantsPath, `${toKebabCase(key)}.js`)
+    // eslint-disable-next-line no-await-in-loop
+    const keyContent = await fs.readFile(keyPath, 'utf8')
+    if (!exportJsonPrimitiveRegExp.test(keyContent)) {
       continue
     }
     const value = require(path.join(constantsPath, `${toKebabCase(key)}.js`))
