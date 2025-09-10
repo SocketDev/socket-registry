2c6ba58805afe60fd8286213a009f4d9787f8422
jdalton <john.david.dalton@gmail.com>
2025-09-10 07:31:17 -0400

    Constants use cleanup

--- scripts/update-licenses.js
+++ scripts/update-licenses.js
@@ -6,13 +6,13 @@ const constants = require('@socketregistry/scripts/constants')
 const { globStreamLicenses } = require('@socketsecurity/registry/lib/globs')
 const { parallelEach } = require('@socketsecurity/registry/lib/streams')
 
-const { LICENSE, LICENSE_CONTENT, UTF8, ignoreGlobs, rootPath } = constants
+const { LICENSE, LICENSE_CONTENT, UTF8 } = constants
 
 void (async () => {
-  const stream = globStreamLicenses(rootPath, {
+  const stream = globStreamLicenses(constants.rootPath, {
     recursive: true,
     ignoreOriginals: true,
-    ignore: [LICENSE, 'scripts/templates', ...ignoreGlobs]
+    ignore: [LICENSE, 'scripts/templates', ...constants.ignoreGlobs]
   })
   await parallelEach(
     stream,
