98eff856d5163ffc52d123025543a456cf5d186c
jdalton <john.david.dalton@gmail.com>
2025-09-11 08:07:09 -0400

    Add isUrl

--- registry/lib/url.d.ts
+++ registry/lib/url.d.ts
@@ -1,4 +1,7 @@
 declare const Url: {
+  isUrl(value: URL): value is URL
+  isUrl(value: string): value is string
+  isUrl(value: any): value is URL | string
   urlSearchParamAsArray(value: any): string[]
   urlSearchParamAsBoolean(
     value: any,
--- registry/lib/url.js
+++ registry/lib/url.js
@@ -3,6 +3,24 @@
 const BooleanCtor = Boolean
 
 /*@__NO_SIDE_EFFECTS__*/
+function isUrl(value) {
+  const isStr = typeof value === 'string'
+  if (isStr && value === '') {
+    return false
+  }
+  const isObj = !isStr && value !== null && typeof value === 'object'
+  if (!isStr && !isObj) {
+    return false
+  }
+  try {
+    // eslint-disable-next-line no-new
+    new URL(value)
+    return true
+  } catch {}
+  return false
+}
+
+/*@__NO_SIDE_EFFECTS__*/
 function urlSearchParamAsArray(value) {
   return typeof value === 'string'
     ? value.trim().split(/, */).filter(BooleanCtor)
@@ -22,6 +40,7 @@ function urlSearchParamAsBoolean(value, defaultValue = false) {
 }
 
 module.exports = {
+  isUrl,
   urlSearchParamAsArray,
   urlSearchParamAsBoolean
 }
