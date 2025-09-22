6b84ec74797aa9c357aa4a6549727b5a4de63ace
jdalton <john.david.dalton@gmail.com>
2025-09-22 19:07:17 -0400

    Lint nits

--- scripts/test.js
+++ scripts/test.js
@@ -79,7 +79,8 @@ void (async () => {
       ...process.env,
       ...(hasForce ? { FORCE_TEST: '1' } : {}),
       // Increase Node.js heap size to prevent out of memory errors in tests.
-      NODE_OPTIONS: `${process.env.NODE_OPTIONS || ''} --max-old-space-size=4096`.trim(),
+      NODE_OPTIONS:
+        `${process.env.NODE_OPTIONS || ''} --max-old-space-size=4096`.trim(),
     }
 
     // Handle Windows vs Unix for vitest executable.
