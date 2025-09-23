e1eb7ad1c85f627118330b42b41db09b9671c29d
jdalton <john.david.dalton@gmail.com>
2025-09-23 17:41:25 -0400

    Add lib/signal-exit

--- registry/lib/constants/abort-controller.js
+++ registry/lib/constants/abort-controller.js
@@ -3,12 +3,12 @@
 // The 'signal-exit' package is browser safe.
 // Do NOT defer loading, otherwise mystery errors may occur at the end of the
 // event loop.
-const signalExit = /*@__PURE__*/ require('../../external/signal-exit')
+const { onExit } = /*@__PURE__*/ require('../signal-exit')
 
 const abortController = new AbortController()
 
 // Detect ^C, i.e. Ctrl + C.
-signalExit.onExit(() => {
+onExit(() => {
   abortController.abort()
 })
 
