c0b1d9ab3d1b32ded928af55f370c011b3eb33d3
jdalton <john.david.dalton@gmail.com>
2025-09-29 08:10:48 -0400

    Fix tsgo destructuring bug in all lib files
    Replace destructuring from global objects with direct property access
    to work around tsgo transpilation bug that causes runtime errors.
    References: https://github.com/SocketDev/socket-packageurl-js/issues/3

--- registry/src/lib/json.ts
+++ registry/src/lib/json.ts
@@ -23,7 +23,11 @@ export interface JsonParseOptions {
   throws?: boolean
 }
 
-const { parse: JSONParse } = JSON
+// IMPORTANT: Do not use destructuring here - use direct assignment instead.
+// tsgo has a bug that incorrectly transpiles destructured exports, resulting in
+// `exports.SomeName = void 0;` which causes runtime errors.
+// See: https://github.com/SocketDev/socket-packageurl-js/issues/3
+const JSONParse = JSON.parse
 
 /**
  * Check if a value is a Buffer instance.
--- registry/src/lib/packages.ts
+++ registry/src/lib/packages.ts
@@ -74,8 +74,16 @@ export type PacoteOptions = {
   fullMetadata?: boolean | undefined
 }
 
-const { isArray: ArrayIsArray } = Array
-const { hasOwn: ObjectHasOwn } = Object
+// IMPORTANT: Do not use destructuring here - use direct assignment instead.
+// tsgo has a bug that incorrectly transpiles destructured exports, resulting in
+// `exports.SomeName = void 0;` which causes runtime errors.
+// See: https://github.com/SocketDev/socket-packageurl-js/issues/3
+const ArrayIsArray = Array.isArray
+// IMPORTANT: Do not use destructuring here - use direct assignment instead.
+// tsgo has a bug that incorrectly transpiles destructured exports, resulting in
+// `exports.SomeName = void 0;` which causes runtime errors.
+// See: https://github.com/SocketDev/socket-packageurl-js/issues/3
+const ObjectHasOwn = Object.hasOwn
 
 const LOOP_SENTINEL = /*@__PURE__*/ require('./constants/LOOP_SENTINEL').default
 const NPM_REGISTRY_URL =
