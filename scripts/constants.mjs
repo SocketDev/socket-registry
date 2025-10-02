5432fc1be357e47a99ce998c2bf1d8b0d584a565
jdalton <john.david.dalton@gmail.com>
2025-10-02 17:53:51 -0400

    Make skip list dynamic by detecting test files in test/npm

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -177,63 +177,25 @@ const lazyRelTestNpmNodeModulesPath = () =>
 
 const lazySkipTestsByEcosystem = () => {
   const { WIN32 } = registryConstants
-  return new Map([
-    [
-      'npm',
-      new Set([
-        // @hyrious/bun.lockb has no unit tests.
-        // https://github.com/hyrious/bun.lockb/tree/v0.0.4
-        '@hyrious/bun.lockb',
-        'hyrious__bun.lockb',
-        // abab tests use old assert package that depends on util.isError which was
-        // deprecated and removed in Node.js 20+.
-        // https://nodejs.org/docs/latest-v18.x/api/util.html#deprecated-apis
-        'abab',
-        // aggregate-error tests use xo which depends on core-assert which uses
-        // util.isDate that was deprecated and removed in Node.js 20+.
-        // https://nodejs.org/docs/latest-v18.x/api/util.html#deprecated-apis
-        'aggregate-error',
-        // Our array-flatten override supports v1, v2, and v3 APIs, so we handle
-        // testing ourselves.
-        'array-flatten',
-        // date tests fail for some Node versions and platforms, but pass in CI
-        // Win32 environments for the time being.
-        // https://github.com/es-shims/Date/issues/3
-        // https://github.com/es-shims/Date/tree/v2.0.5
-        ...(WIN32 ? [] : ['date']),
-        // es6-object-assign has no unit tests.
-        // https://github.com/rubennorte/es6-object-assign/tree/v1.1.0
-        'es6-object-assign',
-        // harmony-reflect has known failures in its package and requires running
-        // tests in browser.
-        // https://github.com/tvcutsem/harmony-reflect/tree/v1.6.2/test
-        'harmony-reflect',
-        // indent-string tests use xo which depends on core-assert which uses
-        // util.isDate that was deprecated and removed in Node.js 20+.
-        // https://nodejs.org/docs/latest-v18.x/api/util.html#deprecated-apis
-        'indent-string',
-        // is-interactive tests use xo which depends on core-assert which uses
-        // util.isDate that was deprecated and removed in Node.js 20+.
-        // https://nodejs.org/docs/latest-v18.x/api/util.html#deprecated-apis
-        'is-interactive',
-        // is-regex tests don't account for `is-regex` backed by
-        // `require('node:util/types).isRegExp` which triggers no proxy traps and
-        // assumes instead that the 'getOwnPropertyDescriptor' trap will be triggered
-        // by `Object.getOwnPropertyDescriptor(value, 'lastIndex')`.
-        // https://github.com/inspect-js/is-regex/issues/35
-        // https://github.com/inspect-js/is-regex/blob/v1.1.4/test/index.js
-        'is-regex',
-        // json-stable-stringify tests timeout during CI due to recursive tests and memory limits.
-        'json-stable-stringify',
-        // safer-buffer tests assume Buffer.alloc, Buffer.allocUnsafe, and
-        // Buffer.allocUnsafeSlow throw for a size of 2 * (1 << 30), i.e. 2147483648,
-        // which is no longer the case.
-        // https://github.com/ChALkeR/safer-buffer/issues/16
-        // https://github.com/ChALkeR/safer-buffer/blob/v2.1.2/tests.js
-        'safer-buffer',
-      ]),
-    ],
+
+  // Get all test files from test/npm directory.
+  const testNpmPath = lazyTestNpmPath()
+  const testFiles = fs
+    .readdirSync(testNpmPath)
+    .filter(name => name.endsWith('.test.mts'))
+    .map(name => name.slice(0, -'.test.mts'.length))
+
+  const skipSet = new Set([
+    // date tests fail for some Node versions and platforms, but pass in CI
+    // Win32 environments for the time being.
+    // https://github.com/es-shims/Date/issues/3
+    // https://github.com/es-shims/Date/tree/v2.0.5
+    ...(WIN32 ? [] : ['date']),
+    // Dynamically include all packages with test files in test/npm.
+    ...testFiles,
   ])
+
+  return new Map([['npm', skipSet]])
 }
 
 const lazyWin32EnsureTestsByEcosystem = () => {
