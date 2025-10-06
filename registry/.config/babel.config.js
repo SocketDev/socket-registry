6afe0767941983200a01175046ca3d292c5bc489
jdalton <john.david.dalton@gmail.com>
2025-10-05 21:20:36 -0400

    Update Babel config with new plugins

--- registry/.config/babel.config.js
+++ registry/.config/babel.config.js
@@ -3,6 +3,7 @@
 const path = require('node:path')
 
 const rootPath = path.join(__dirname, '..')
+const pluginsPath = path.join(rootPath, 'plugins')
 const scriptsPath = path.join(rootPath, 'scripts')
 const babelPluginsPath = path.join(scriptsPath, 'babel')
 
@@ -20,7 +21,17 @@ module.exports = {
         version: '^7.27.1',
       },
     ],
+    path.join(pluginsPath, 'babel-plugin-inline-require-calls.js'),
     path.join(babelPluginsPath, 'transform-set-proto-plugin.mjs'),
     path.join(babelPluginsPath, 'transform-url-parse-plugin.mjs'),
+  ],
+  presets: [
+    [
+      '@babel/preset-typescript',
+      {
+        allowDeclareFields: true,
+        onlyRemoveTypeImports: true,
+      },
+    ],
   ],
 }
