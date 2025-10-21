ed7ecd5b678398acbd3a82745538d9f0ae673f13
jdalton <john.david.dalton@gmail.com>
2025-10-21 07:15:57 -0400

    Remove unused babel plugins and config
    - Remove plugins/ directory (not published or used)
    - Remove babel.config.js (using esbuild, not babel)
    - Build verified to work without them

--- registry/plugins/babel-plugin-inline-require-calls.d.ts
+++
@@ -1,7 +0,0 @@
-/** @fileoverview Type declarations for babel-plugin-inline-require-calls. */
-
-import type { PluginObj, PluginPass } from '@babel/core'
-
-export default function inlineRequireCalls(babel: {
-  types: typeof import('@babel/types')
-}): PluginObj<PluginPass>
